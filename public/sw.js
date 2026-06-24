19e745a4f2285ad2e26f8d456f586b7188b593e2
INSTITUT TEKNOLOGI UNGGAS <itumelaka@gmail.com>
2026-06-24 19:15:54 +0800

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,24 +1,18 @@
-const CACHE_NAME = "jamsolat-v1";
-const ASSETS = ["/", "/index.html"];
+const CACHE_NAME = "jamsolat-v2";
 
-self.addEventListener("install", (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+self.addEventListener("install", (event) => {
   self.skipWaiting();
 });
 
-self.addEventListener("activate", (e) => {
-  e.waitUntil(
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
     caches.keys().then((keys) =>
-      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+      Promise.all(keys.map((key) => caches.delete(key)))
     )
   );
   self.clients.claim();
 });
 
-self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request).then((cached) => {
-      return cached || fetch(e.request).catch(() => caches.match("/"));
-    })
-  );
+self.addEventListener("fetch", (event) => {
+  event.respondWith(fetch(event.request));
 });
